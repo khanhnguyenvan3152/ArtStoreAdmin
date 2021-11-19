99a8ed3aa2976253b9879ef6461c5040717e4de1
khanhnguyenvan3152 <54436766+khanhnguyenvan3152@users.noreply.github.com>
2021-11-19 13:37:20 +0700

    add default view value to productschema

--- hoacudaden/models/product.js
+++ hoacudaden/models/product.js
@@ -17,7 +17,8 @@ const ProductSchema = new mongoose.Schema({
     type: String,
     tags: Array,
     images: Array,
-    description:String
+    description:String,
+    view: {type:Number,default:0}
 },{timestamps:true})
 
 module.exports = mongoose.model('product',ProductSchema)
